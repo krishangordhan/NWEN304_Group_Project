14cf3c135ee703f72f3864ad0a56bd1ce81a48d4
krishangordhan <krishan_gord@hotmail.com>
2017-06-25 14:58:51 +1200

    Krishan Gordhan 25/06/2017 Just working on search

--- index.js
+++ index.js
@@ -9,33 +9,33 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 //connecting to Postgres locally
-var connectionString= "postgres://dtxszhltugvtsq:aef48216097f039b8eae0cb71c9f2f65a1c099160f9686b8681ee3a10b48a95b@ec2-50-19-218-160.compute-1.amazonaws.com:5432/d5d77orodbj15i";
+var connectionString = "postgres://dtxszhltugvtsq:aef48216097f039b8eae0cb71c9f2f65a1c099160f9686b8681ee3a10b48a95b@ec2-50-19-218-160.compute-1.amazonaws.com:5432/d5d77orodbj15i";
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
 app.get('/', function (req, res) {
- res.send('Hello World!');
+    res.send('Hello World!');
 });
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
 
-        // render the page and pass in any flash data if it exists
-        res.render('login.html', { message: req.flash('loginMessage') }); 
-    });
- // process the login form
- // app.post('/login', do all our passport stuff here);
-app.get('/Register', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('login.html', { message: req.flash('loginMessage') });
+});
+// process the login form
+// app.post('/login', do all our passport stuff here);
+app.get('/Register', function (req, res) {
 
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
 // process the signup form
 // app.post('/signup', do all our passport stuff here);
-app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
+app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('profile.ejs', {
+        user: req.user // get the user out of session and pass to template
     });
+});
 function isLoggedIn(req, res, next) {
 
     // if user is authenticated in the session, carry on 
@@ -45,25 +45,44 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
-	
+
 app.listen(port, function () {
- console.log('Example app listening on port 8080!');
-}); 
+    console.log('Example app listening on port 8080!');
+});
 
 
 
 //Setup for postgres db requires ssl connection
 
 pg.defaults.ssl = true;
-pg.connect(connectionString, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+pg.connect(connectionString, function (err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
+    client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .on('row', function (row) {
+            console.log(JSON.stringify(row));
+        });
+});
+
+app.post('/search', function (req, res) {
+    var searchs = req.query.search;
+    var query = cient.query("SELECT * FROM products WHERE name = '" + searchs + "' OR author = '" + searchs + "' OR isbn = '" + searchs + "'");
+    var results = [];
+    console.log("grabbing database to search");
+    query.on('row', function (row) {
+        results.push(row);
     });
+    query.on('end', function () {
+        client.end();
+        response.json(results);
+    });
+
+
+});
+app.get('/request', function (req, res) {
+
 });
 
 
--- search.js
+++ search.js
@@ -1,6 +1,27 @@
 $(document).ready(function(e){
 var searchTerm = parseSearch();
 
+var searchResult = getSearch();
+
+
+
+function getSearch(){
+    var searchurl = "https://boiling-eyrie-15862.herokuapp.com/search?search=" + searchTerm;
+    $.ajax({
+        method: 'POST',
+        url: searchurl,
+        data: JSON.stringify({
+           search: searchTerm
+        }),
+        success: function(data){
+            return (data);
+        },
+        error: function(){
+
+        }
+    });
+}
+
 function parseSearch(){
         var url = document.URL;
         var queryStart = url.indexOf("="),
@@ -8,5 +29,7 @@ function parseSearch(){
             query = url.slice(queryStart, queryEnd - 1);
         return query;
     }
+
+
 
 });
