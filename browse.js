fbde839e5b9499543aa941e8f4ab9d04b9e30453
corkesimon <corkesimon@gmail.com>
2017-06-26 23:02:58 +1200

    Added function to browse page buttons
    Made it so clicking on the next previos buttons updates the session storage so as to change the page number

---
+++ browse.js
@@ -0,0 +1,67 @@
+$(document).ready(function(e){
+
+		window.onload = function(){
+			if(sessionStorage.page){
+				if(sessionStorage.page == 6){
+					var n = document.getElementById("Next");
+					if(n != null){
+						n.remove();
+					}
+					n = document.getElementById("prev");
+					if(n == null){
+						var prev = '<button id = "prev" onclick = "prev()" type ="button" class="w3-button w3-black w3-padding-large w3-large w3-margin-top"></button';
+           			var $prev = $(prev);
+           			$('#footer').prepend($prev);
+           			document.getElementById("prev").innerHTML = "Prev";
+					}
+				}
+			}
+			else{
+				sessionStorage.page = 1;
+			}
+		}
+
+		window.next = function(){
+        if (sessionStorage.page) {
+        	console.log(sessionStorage);
+        	if(sessionStorage.page == 1){
+           		var prev = '<button id = "prev" onclick = "prev()" type ="button" class="w3-button w3-black w3-padding-large w3-large w3-margin-top"></button';
+           		var $prev = $(prev);
+           		$('#footer').prepend($prev);
+           		document.getElementById("prev").innerHTML = "Prev";
+           		sessionStorage.page = Number(sessionStorage.page) + 1;
+       		}
+       		else if((Number(sessionStorage.page) > 1) && (Number(sessionStorage.page) < 5 )){
+       			sessionStorage.page = Number(sessionStorage.page) + 1;
+       		}
+       		else if(sessionStorage.page == 5){
+       			sessionStorage.page = Number(sessionStorage.page) + 1;
+       			$('#Next').remove();
+
+       		}
+
+        }
+	}
+
+	window.prev = function(){
+        if (sessionStorage.page) {
+        	console.log(sessionStorage);
+        	if(sessionStorage.page == 6){
+           		var next = '<button id = "Next" onclick = "next()" type ="button" class="w3-button w3-black w3-padding-large w3-large w3-margin-top"></button';
+           		var $next = $(next);
+           		$('#prev').after($next);
+           		document.getElementById("Next").innerHTML = "Next";
+           		sessionStorage.page = Number(sessionStorage.page) - 1;
+       		}
+       		else if((Number(sessionStorage.page) > 2) && (Number(sessionStorage.page) < 6 )){
+       			sessionStorage.page = Number(sessionStorage.page) - 1;
+       		}
+       		else if(sessionStorage.page == 2){
+       			sessionStorage.page = Number(sessionStorage.page) - 1;
+       			$('#prev').remove();
+       		}
+
+        }
+	}
+
+});
