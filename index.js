6b55774b3e8c3515742f249cccc0e01e78c07278
astrosheen <astrosheen@gmail.com>
2017-07-04 03:04:46 +1200

    .

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({	extended: true})); // get information from html forms
 app.use(bodyParser.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/')));
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
 
