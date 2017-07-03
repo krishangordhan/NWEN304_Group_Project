a126a8ec4ee610f29e343e308d53d3f202bb52e6
astrosheen <astrosheen@gmail.com>
2017-07-04 02:25:46 +1200

    fix

--- index.js
+++ index.js
@@ -140,4 +140,4 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport*/
->>>>>>> Working
+
