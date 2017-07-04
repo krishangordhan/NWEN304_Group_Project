289b727e6b3b9cd3da720cafe053c00128dc976b
astrosheen <astrosheen@gmail.com>
2017-07-05 11:08:39 +1200

    heroku attempt 2

--- routes.js
+++ routes.js
@@ -66,7 +66,7 @@ module.exports = function(app, passport) {
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/checkout', isLoggedIn, function(req, res) {
-		res.sendFile(__dirname + 'checkout.html', {
+		res.sendFile(__dirname + '/checkout.html', {
 			user : req.user // get the user out of session and pass to template
 		});
 	});
